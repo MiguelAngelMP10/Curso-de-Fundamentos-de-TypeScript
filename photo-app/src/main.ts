78509a431747ef16091a46d616257917b8903ee3
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-07-21 15:38:20 -0500

    Resumen de lo aprendido

---
+++ photo-app/src/main.ts
@@ -0,0 +1,16 @@
+import { User, Album, Picture, PhotoOrientation } from "./photo-app";
+const user = new User(1, "Miguel Angel", "Migue", true);
+const album = new Album(10, "Platzi Pictures");
+const picture = new Picture(
+  1,
+  "TypeScript course",
+  "2020-03",
+  PhotoOrientation.Landscape
+);
+
+//creamos relaciones
+
+user.addAlbum(album);
+album.addPicture(picture);
+
+console.log("user", user);
